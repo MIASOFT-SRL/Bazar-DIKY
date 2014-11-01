df5317ee844e4d36791df8f99b5332f70285541f
Miguel Aldo Balderrama Vaca <miguel.aldobv@gmail.com>
2014-11-01 13:11:39 -0400

    20141101 se mejoro el estilo del menu al hacer scroll v2

--- js/menu-scroll.js
+++ js/menu-scroll.js
@@ -4,15 +4,18 @@
 $(document).ready(function() {
   var contenedor = $('#menu-contenedor');
   var image_contenedor = $('#image-contenedor');
+  var menu = $('#menu');
   // Cada vez que se haga scroll en la página
   // haremos un chequeo del estado del menú
   $(window).on('scroll', function() {
     if($(window).scrollTop() > 140) {
       contenedor.addClass('MenuPrincipalScroll');
       image_contenedor.addClass('MenuPrincipal-imageContainerScroll');
+      menu.addClass('MenuPrincipal-listaScroll');
     } else {
       contenedor.removeClass('MenuPrincipalScroll');
       image_contenedor.removeClass('MenuPrincipal-imageContainerScroll');
+      menu.removeClass('MenuPrincipal-listaScroll');
     }
   });
 });
